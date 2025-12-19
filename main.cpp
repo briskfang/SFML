f5aea83165be67832c57e93e2eb0fe94c87d88ca
briskfang <briskfang4me@gmail.com>
2025-12-19 11:32:43 -0500

    first background displayed

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 
+/*
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
@@ -20,3 +21,40 @@ int main()
         window.display();
     }
 }
+*/
+
+
+
+using namespace sf;
+
+int main()
+{
+    RenderWindow window(VideoMode({1440, 900}), "Timber!");
+
+    Texture textureBackgound;
+    textureBackgound.loadFromFile("graphics/background.png");
+
+    Sprite spriteBackground;
+    spriteBackground.setTexture(textureBackgound);
+    spriteBackground.setPosition(0, 0);
+
+
+    while (window.isOpen())
+    {
+        
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+        
+        window.clear();
+        window.draw(spriteBackground);
+        window.display();
+    }
+    
+
+    return 0;    
+}
+
