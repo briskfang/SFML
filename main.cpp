d573cfca41d85e1d5ab85e919253d8f8afc9cbc8
briskfang <briskfang4me@gmail.com>
2025-12-19 18:25:38 -0500

    add branches

--- main.cpp
+++ main.cpp
@@ -29,6 +29,43 @@ int main()
 
 using namespace sf;
 
+void updateBranches(int seed);
+
+const int NUM_BRANCHES = 6;
+Sprite branches[NUM_BRANCHES]; //how to implement the construct to support this?
+enum class side{ LEFT, RIGHT, NONE};
+side branchPositions[NUM_BRANCHES];
+
+
+
+void updateBranches(int seed)
+{
+    for(int j = NUM_BRANCHES - 1; j > 0; j--)
+    {
+        branchPositions[j] = branchPositions[j-1]; 
+    }
+
+    srand((int)time(0) + seed);
+    int r = (rand() % 5);
+
+    switch(r)
+    {
+        case 0:
+            branchPositions[0] = side::LEFT;
+            break;
+        case 1:
+            branchPositions[0] = side::RIGHT;
+            break;
+        default:
+            branchPositions[0] = side::NONE;
+            break;
+    }
+
+
+}
+
+
+
 int main()
 {
     RenderWindow window(VideoMode({1440, 900}), "Timber!");
@@ -111,6 +148,23 @@ int main()
     messageText.setOrigin(textRect.left + textRect.width/2.0f, textRect.top + textRect.height/2.0f);
     messageText.setPosition(1440/2.0f, 900/2.0f);
     scoreText.setPosition(20, 20);
+
+    Texture textureBranch;
+    textureBranch.loadFromFile("graphics/branch.png");
+
+    for(int i = 0; i < NUM_BRANCHES; i++)
+    {
+        branches[i].setTexture(textureBranch);
+        branches[i].setPosition(-2000, -2000);
+        branches[i].setOrigin(220, 20);
+    }
+
+    updateBranches(1);
+    updateBranches(2);
+    updateBranches(3);
+    updateBranches(4);
+    updateBranches(5);
+
 
 
 
@@ -232,6 +286,29 @@ int main()
             ss << "Score = " << score;
             scoreText.setString(ss.str());
 
+
+            for(int i = 0; i < NUM_BRANCHES; i++)
+            {
+                float height = i * 150;
+                if(branchPositions[i] == side::LEFT)
+                {
+                    branches[i].setPosition(500, height);
+                    branches[i].setRotation(180);
+                }
+                else if(branchPositions[i] == side::RIGHT)
+                {
+                    branches[i].setPosition(1200, height);
+                    branches[i].setRotation(0);
+                }
+                else
+                {
+                    branches[i].setPosition(3000, height);
+                }
+
+            }
+
+
+
         }
 
         
@@ -240,8 +317,13 @@ int main()
         window.draw(spriteCloud1);
         window.draw(spriteCloud2);
         window.draw(spriteCloud3);
+        for(int i = 0; i < NUM_BRANCHES; i++)
+        {
+            window.draw(branches[i]);
+        }
+
         window.draw(spriteTree);
-        window.draw(spriteBee);
+        window.draw(spriteBee); 
         window.draw(scoreText);
         window.draw(timeBar);
         //window.display();
